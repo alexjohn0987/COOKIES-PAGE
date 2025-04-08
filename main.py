0cc2b816ab315a8a3e2fa66eb9cf58aa31ec6386
Alex john <rajmishra8521.06@gmail.com>
2025-04-08 07:12:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -324,6 +324,5 @@ def get_active_tasks():
         })
 
 if __name__ == '__main__':
-    os.makedirs('uploads', exist_debug=True)
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    
+    app.run(host='0.0.0.0', port=5000)
